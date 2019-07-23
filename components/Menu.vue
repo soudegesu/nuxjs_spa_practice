baf74acadb096769e4a430daead0a7257205cdc4
soudegesu <soudegesu@hotmail.co.jp>
2019-07-23 15:41:48 +0900

    add to home link

--- components/Menu.vue
+++ components/Menu.vue
@@ -3,7 +3,12 @@
     <aside class="menu">
       <p class="menu-label">Menu</p>
       <ul class="menu-list">
-        <li><nuxt-link to="/next">to next</nuxt-link></li>
+        <li>
+          <nuxt-link to="/">to home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/next">to next</nuxt-link>
+        </li>
       </ul>
     </aside>
   </div>
