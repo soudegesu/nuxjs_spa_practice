9740bc91de08dd58f4380951e8d54d40ed89e67e
soudegesu <soudegesu@hotmail.co.jp>
2019-07-23 16:28:31 +0900

    test build with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
 module.exports = {
+  preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'vue',
+    'json'
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.ts?$': 'ts-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
