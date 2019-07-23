7dfdb8e07bfc9123bda507f93d9d0f91df0a4e53
soudegesu <soudegesu@hotmail.co.jp>
2019-07-23 21:43:24 +0900

    async request sample implement

--- pages/next.vue
+++ pages/next.vue
@@ -5,6 +5,11 @@
       <Menu />
     </div>
     <Msg :message="initMsg" />
+    <div class="buttons are-medium">
+      <a class="button is-primary is-outlined" @click="regist('A')">A</a>
+      <a class="button is-link is-outlined" @click="regist('B')">B</a>
+      <a class="button is-info is-outlined" @click="regist('C')">C</a>
+    </div>
   </div>
 </template>
 
@@ -20,6 +25,13 @@ import { Vue, Component } from 'vue-property-decorator'
   data() {
     return {
       initMsg: 'Welcome to netx page!!'
+    }
+  },
+  methods: {
+    async regist(target) {
+      window.console.log(target)
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      window.console.log(ip)
     }
   }
 })
